60ba870f90ffb697e81ab1492d6585919e6fdd92
Maksim Shokin <maksshokin90@gmail.com>
2024-08-04 20:50:30 +0300

    uopi

--- ya_news/news/pytest_tests/conftest.py
+++ ya_news/news/pytest_tests/conftest.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.test import Client
-from django.urls import reverse
 from django.utils import timezone
 
 import pytest
