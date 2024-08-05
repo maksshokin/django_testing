8ae684821e191567b4e33f07a2a2e9536035272e
Maksim Shokin <maksshokin90@gmail.com>
2024-08-06 00:02:08 +0300

    8

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 
 import pytest
 
+from news.forms import CommentForm
+
 AUTHOR_CLIENT = pytest.lazy_fixture('author_client')
 CLIENT = pytest.lazy_fixture('client')
 NEWS_DETAIL_URL = 'news:detail'
@@ -58,4 +60,4 @@ def test_form_availability_for_different_clients(
     assert ('form' in response.context) is note_in_list
     if note_in_list:
         form = type(response.context['form'])
-        assert form == 'CommentForm'
+        assert form == type(CommentForm)
